7f9c9c829e02d83f50a3f8b181fbf70a783d1269
Sbelgana <44445774+Sbelgana@users.noreply.github.com>
2024-12-02 11:12:04 -0500

    Update and rename back_ground_rema.js to back_ground_remax.js

--- back_ground_remax.js
+++ back_ground_remax.js
@@ -423,7 +423,7 @@ function checkBounds(x, y) {
 }
 
 function createCanvas() {
-  container = document.querySelector('.c-iLVmPU .c-ealvbK');
+  container = document.querySelector('.c-ealvbK');
 	canvas = {
 		a: document.createElement('canvas'),
 		b: document.createElement('canvas')
